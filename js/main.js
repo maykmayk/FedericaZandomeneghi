5029fd76a0a0c6307ff5a8af1840095511897249
maykmayk <m.devita@ied.edu>
2023-08-30 21:33:25 +0200

    -

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 const _app = {};
 
+_app.randomImgHome = () => {
+    let cont = document.getElementById("imgHome"); // Get the HTML element by ID
+    let images = [
+        "./asset/images/zando.png",
+        "./asset/images/zando2.png",
+    ];
+    let randomIndex = Math.floor(Math.random() * images.length); // Generate a random index
+    cont.src = images[randomIndex]; // Set the src attribute to the randomly selected image URL
+};
+
+
 _app.header = () => {
     let index = document.getElementById("index")
     let works = document.getElementById("works")
     let about = document.getElementById("about")
     if (window.location.pathname.includes("index.html")) {
+        _app.randomImgHome();
         index.classList.add("underline")
         works.classList.remove("underline")
         about.classList.remove("underline")
@@ -22,7 +34,7 @@ _app.header = () => {
 };
 
 _app.startUp = () => {
-    _app.header()
+    _app.header();
 };
 
 _app.startUp();
